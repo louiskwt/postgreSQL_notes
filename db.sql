d52ae34e937ed8e64f086be2648b6e315f289a13
Louis Tsang <kawingt2@gmail.com>
2022-05-14 23:35:38 +0800

    CURD Queries

--- db.sql
+++ db.sql
@@ -19,3 +19,30 @@ DROP TABLE products;
 
 -- Delete DB
 DROP DATABASE practice;
+
+
+-- Yelp Table
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+-- Create entry into DB table
+INSERT INTO restaurants (name, location, price_range)
+VALUES ('Pizza Hut', 'San Jose', 2);
+
+-- CREATE entry with returning
+INSERT INTO restaurants (name, location, price_range)
+VALUES ('Mos Burger', 'San Francisco', 4) returning *;
+
+--Update entry
+UPDATE restaurants 
+SET name = 'Panda Express', 
+    location = 'Boston',
+    price_range = 2
+WHERE id = 7;
+
+--Delete query
+DELETE FROM restaurants where id = 8;
